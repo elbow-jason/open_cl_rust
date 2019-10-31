76f6b0b22b5595274682d58f5296bce82cab7bbc
Jason Goldberger <jlgoldb2@asu.edu>
2019-10-31 16:05:50 -0700

    cleaned Program. added Clone.

--- opencl_bindings/src/program.rs
+++ opencl_bindings/src/program.rs
@@ -12,14 +12,11 @@ use crate::open_cl::{
     cl_create_kernel,
     cl_get_program_build_log,
     cl_release_program,
+    cl_retain_program,
     ClObject,
 };
 
 use crate::{Device, Kernel, Output, Context};
-
-fn do_build_on_devices(program: &Program, devices: &[&Device]) -> Output<()> {
-    cl_build_program(program, devices)
-}
 
 #[repr(C)]
 #[derive(Debug, Eq, PartialEq, Hash)]
@@ -33,6 +30,18 @@ impl Drop for Program {
         unsafe { cl_release_program(&self.raw_cl_object()) };
     }
 }
+
+impl Clone for Program {
+    fn clone(&self) -> Program {
+        unsafe {
+            let cl_object: cl_program = self.raw_cl_object();
+            cl_retain_program(&cl_object);
+            Program::new(cl_object)
+        }
+    }
+}
+
+
 impl ClObject<cl_program> for Program {
     unsafe fn raw_cl_object(&self) -> cl_program {
         self.inner
@@ -56,12 +65,11 @@ impl Program {
     }
 
     pub fn build_on_many_devices(&self, devices: &[&Device]) -> Output<()> {
-        do_build_on_devices(self, devices)
+        cl_build_program(self, devices)
     }
 
     pub fn build_on_one_device(&self, device: &Device) -> Output<()> {
-        let devices = vec![device];
-        do_build_on_devices(self, &devices)
+        cl_build_program(self, &vec![device])
     }
 
     pub fn fetch_kernel(&self, name: &str) -> Output<Kernel> {
@@ -75,35 +83,37 @@ impl Program {
         cl_get_program_build_log(
             program,
             device,
-            ProgramBuildInfo::LOG as cl_program_build_info,
+            ProgramBuildInfo::Log as cl_program_build_info,
         )
     }
 }
 
 crate::__codes_enum!(ProgramBuildInfo, cl_program_build_info, {
-    STATUS => 0x1181,
-    OPTIONS => 0x1182,
-    LOG => 0x1183,
-    BINARYTYPE => 0x1184,
-    GLOBALVARIABLETOTALSIZE => 0x1185
+    Status => 0x1181,
+    Options => 0x1182,
+    Log => 0x1183,
+    // NOTE: Version for BinaryType?
+    // BinaryType => 0x1184,
+    GlobalVariableTotalSize => 0x1185
 });
 
 crate::__codes_enum!(ProgramInfo, cl_program_info, {
-    REFERENCE_COUNT => 0x1160,
-    CONTEXT => 0x1161,
-    NUM_DEVICES => 0x1162,
-    DEVICES => 0x1163,
-    SOURCE => 0x1164,
-    BINARY_SIZES => 0x1165,
-    BINARIES => 0x1166,
-    NUM_KERNELS => 0x1167,
-    KERNEL_NAMES => 0x1168,
-    IL => 0x1169,
-    SCOPE_GLOBAL_CTORS_PRESENT => 0x116A,
-    SCOPE_GLOBAL_DTORS_PRESENT => 0x116B
+    ReferenceCount => 0x1160,
+    Context => 0x1161,
+    NumDevices => 0x1162,
+    Devices => 0x1163,
+    Source => 0x1164,
+    BinarySizes => 0x1165,
+    Binaries => 0x1166,
+    NumKernels => 0x1167,
+    KernelNames => 0x1168,
+    Il => 0x1169,
+    ScopeGlobalCtorsPresent => 0x116A,
+    ScopeGlobalDtorsPresent => 0x116B
 
 });
 
+// NOTE: Version for cl_program_binary_type?
 // crate::__codes_enum!(ProgramBinaryType, cl_program_binary_type, {
 //     NONE => 0x0,
 //     COMPILED_OBJECT => 0x1,
