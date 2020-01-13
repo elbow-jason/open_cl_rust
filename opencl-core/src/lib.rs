7ab4c54e7bc2d890ec6bf5e04eb981fa9ee6b903
Jason Goldberger <jlgoldb2@asu.edu>
2020-01-12 21:26:22 -0700

    added env_logger for testing

--- opencl-core/src/lib.rs
+++ opencl-core/src/lib.rs
@@ -4,20 +4,18 @@
 /// kernel and memory objects. OpenCL API calls that queue commands to a
 /// command-queue or change the state of OpenCL objects such as command-queue
 /// objects, memory objects, program and kernel objects are not thread-safe.
-///
 
-#[macro_use]
-extern crate lazy_static;
+#[allow(unused_imports)]
+#[macro_use] extern crate log;
 
-#[macro_use]
-extern crate bitflags;
+#[macro_use] extern crate lazy_static;
 
-#[macro_use]
-extern crate failure;
+#[macro_use] extern crate bitflags;
+
+#[macro_use] extern crate failure;
 
 // macros are in there keep it first. order matters.
-#[macro_use]
-mod macros;
+#[macro_use] mod macros;
 
 // pub mod utils;
 
@@ -61,6 +59,7 @@ mod tests;
 mod testing {
     #![allow(dead_code)]
     use crate::*;
+    use std::sync::RwLock;
     
     pub fn src_buffer_plus_one() -> &'static str {
         "__kernel void test(__global int *i) { *i += 1; }"
@@ -91,5 +90,33 @@ mod testing {
             .unwrap_or_else(|e| panic!("Failed to list all devices: {:?}", e));
         assert!(devices.len() > 0);
         devices.remove(0)
+    }
+
+    lazy_static! {
+        static ref LOG_INITED: RwLock<bool> = RwLock::new(false);
+    }
+
+    pub fn init_logger() {
+        use log::LevelFilter;
+        let _ = env_logger::builder().is_test(true).filter(None, LevelFilter::Debug).init();
+        let read_lock = LOG_INITED.read().unwrap();
+        if *read_lock == true {
+            return;
+        } else {
+            std::mem::drop(read_lock);
+            let mut write_lock = LOG_INITED.write().unwrap();
+            if *write_lock == false {
+                *write_lock = true;
+            }
+        }
+    }
+
+    #[test]
+    fn logger_init_actually_lets_us_log() {
+        println!("println in in logger_init_actually_lets_us_log");
+        debug!("debug in logger_init_actually_lets_us_log");
+        info!("info in logger_init_actually_lets_us_log");
+        warn!("info in logger_init_actually_lets_us_log");
+        error!("info in logger_init_actually_lets_us_log");
     }
 }
