e7e442b414e81e8818b55ca0d045340718ddd160
Jason Goldberger <jlgoldb2@asu.edu>
2020-05-24 12:04:57 -0700

    updated open_cl_core pub usage; added number_types to open_cl_core

--- open-cl-core/src/lib.rs
+++ open-cl-core/src/lib.rs
@@ -63,10 +63,27 @@ pub use session::Session;
 
 // pub use command_queue::CommandQueue;
 
-pub use ll::cl::{ClObject, CommandQueueProperties, DeviceType, MemFlags, StatusCodeError};
+pub use ll::cl::{
+    ClObject, CommandQueueProperties, DeviceAffinityDomain, DeviceType, MemFlags, StatusCodeError,
+};
 pub use ll::numbers::{Number, NumberType, NumberTyped, NumberTypedT};
+
 pub use ll::vec_or_slice::{MutVecOrSlice, VecOrSlice};
 pub use ll::{
-    ArgPtr, AsPtr, BufferBuilder, CommandQueueOptions, Dims, HostAccess, KernelAccess, KernelArg,
-    KernelOperation, MemAllocation, MemConfig, Waitlist, Work,
+    ArgPtr, AsPtr, BufferBuilder, CommandQueueOptions, Dims, HasDeviceInfo, HostAccess,
+    KernelAccess, KernelArg, KernelOperation, Mem, MemAllocation, MemConfig, MemConfigBuilder,
+    MemPtr, NumCastFrom, NumCastInto, NumberTypeError, Waitlist, Work,
 };
+
+pub mod number_types {
+    pub use crate::ll::numbers::{Char, Char16, Char2, Char4, Char8};
+    pub use crate::ll::numbers::{Double, Double16, Double2, Double4, Double8};
+    pub use crate::ll::numbers::{Float, Float16, Float2, Float4, Float8};
+    pub use crate::ll::numbers::{Int, Int16, Int2, Int4, Int8};
+    pub use crate::ll::numbers::{Long, Long16, Long2, Long4, Long8};
+    pub use crate::ll::numbers::{Short, Short16, Short2, Short4, Short8};
+    pub use crate::ll::numbers::{Uchar, Uchar16, Uchar2, Uchar4, Uchar8};
+    pub use crate::ll::numbers::{Uint, Uint16, Uint2, Uint4, Uint8};
+    pub use crate::ll::numbers::{Ulong, Ulong16, Ulong2, Ulong4, Ulong8};
+    pub use crate::ll::numbers::{Ushort, Ushort16, Ushort2, Ushort4, Ushort8};
+}
